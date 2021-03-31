ccd36c0b35fb016f1f63c0a977acb23528d33d85
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-03-30 23:36:05 -0300

    Ordens de precedência e operadores

---
+++ Módulo B/Aula08/OrdemDePrecedenciaDosOperadores.js
@@ -0,0 +1,9 @@
+/*
+
+    ORDEM DE PRECEDÊNCIA DOS OPERADORES
+
+    1º - Aritméticos
+    2º - Relacionais
+    3º - Lógicos
+
+*/
--- README.md
+++
@@ -1,3 +0,0 @@
-# CursoJS-CursoEmVideo
-Arquivos e códigos usados e criados no curso de Javascript do CursoEmVídeo
-
