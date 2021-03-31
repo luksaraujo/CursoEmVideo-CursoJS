ffeb68c52bfc39a74d5a85d24319640d0d800b33
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-03-30 23:06:01 -0300

    Operadores Aritméticos

---
+++ Aula07/OrdemDePrecedencia.js
@@ -0,0 +1,10 @@
+/*
+
+    ORDEM DE PRECEDÊNCIA DOS OPERADORES ARITIMÉTICOS
+    
+    () = Primeiro é calculado tudo que está entre parêntesis
+    ** = Depois são calculadas as potenciações
+    * / % = Multiplicação, divisão e resto da divisão são calculadas na ordem em que aparecerem, da esquerda pra direita (o que estiver primeiro na linha)
+    + - = Soma e substração são calculadas na ordem em que aparecerem
+
+*/
