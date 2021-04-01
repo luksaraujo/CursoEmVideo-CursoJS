c90f1c9aeb99c9abd175f8f2accc64354ef65c75
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-03-31 22:43:04 -0300

    Atividades e exercício da aula 11
    Condicionais

---
+++ Módulo D/Aula 11/ex009.js
@@ -0,0 +1,6 @@
+var velocidade = 80
+console.log(`A velocidade do seu veículo é de ${velocidade} Km/h.`)
+if (velocidade > 50) {
+    console.log('Você ultrapassou a velocidade máxima permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ Módulo D/Aula 11/ex010.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if (país != 'Brasil') {
+    console.log('Você é estrangeiro')
+} else {
+    console.log('Você é brasileiro')
+}
