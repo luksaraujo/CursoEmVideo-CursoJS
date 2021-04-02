4b2f82ff570c8d87f7264873074074f92a5ffc1e
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-04-02 19:54:55 -0300

    Tabuada

---
+++ Módulo E/Ex02/script.js
@@ -0,0 +1,16 @@
+function tabuada(){
+    let num = document.getElementById('txtNum')
+    let tab = document.getElementById('selTab')
+    if (num.value.length == 0) {
+        window.alert('[ERRO] Você deve digitar um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(var c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
