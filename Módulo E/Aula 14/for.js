e9dc4b717c6c7637bb9b46655e97c3076b011fd5
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-04-02 17:59:52 -0300

    Estrutura For

---
+++ Módulo E/Aula 14/for.js
@@ -0,0 +1,10 @@
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+
+/*
+    A estrutura for, apesar de ter o incremento na primeira linha
+    apenas o faz quando está indo do fechamento do bloco para o
+    teste lógico. Ou seja, primeiro ela executa o bloco, depois
+    faz o incremento.
+*/
