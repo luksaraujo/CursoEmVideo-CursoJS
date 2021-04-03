0da65c0b27e653c1e54e043ee1201d29114f6d22
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-04-02 22:35:07 -0300

    Vetores
    Vetores, atributos, funções e For..In

---
+++ Módulo F/Aula 15/Vetores.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3] //Cria o array/vetor com os valores que estão inseridos dentro dos conchetes
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos/posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+num.sort()
+console.log(`Reordenado, o vetor fica assim: ${num}`)
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+
+pos = num.indexOf(4)
+if (pos == -1) console.log(`O valor 4 não foi encontrado`)
+else console.log(`O valor está na posição ${pos}`)
